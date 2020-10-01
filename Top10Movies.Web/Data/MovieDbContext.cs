0197c43cb62f1a689cbd52930acba8270ee2d9e3
Ryan <nolanr6969@gmail.com>
2020-10-01 13:40:54 +0100

    Added EF Core dependencies and DbContext file within Data folder.

---
+++ Top10Movies.Web/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Top10Movies.Web.Models;
+
+namespace Top10Movies.Web.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieList> MovieLists { get; set; }
+    }
+}
--- Top10Movies.Web/Models/EFTop10MovieRepository.cs
+++ Top10Movies.Web/Models/EFTop10MovieRepository.cs
@@ -7,6 +7,10 @@ namespace Top10Movies.Web.Models
 {
     public class EFTop10MovieRepository : IMovieListRepository
     {
+        public EFTop10MovieRepository()
+        {
+
+        }
         public IEnumerable<Movie> GetAllMoviesInTop10()
         {
             throw new NotImplementedException();

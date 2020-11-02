88ddbbd1c2374bb27edf6aef2f297aad6757292c
Ryan <nolanr6969@gmail.com>
2020-11-02 16:49:09 +0000

    Added a movies action and movies view model.

--- Top10Movies.Web/Controllers/HomeController.cs
+++ Top10Movies.Web/Controllers/HomeController.cs
@@ -12,25 +12,26 @@ namespace Top10Movies.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IMovieApiService _movieApiService;
 
-        public HomeController(ILogger<HomeController> logger,  IMovieApiService movieApiService)
+        public HomeController(ILogger<HomeController> logger, IMovieApiService movieApiService)
         {
             _logger = logger;
             _movieApiService = movieApiService;
         }
 
-        public IActionResult Index(string searchTerm)
+        public IActionResult Index() => View();
+        
+        public IActionResult Movies(string searchTerm)
         {
-            Top10MoviesListViewModel ViewModel = null;
+            MoviesViewModel viewModel = null;
             if (!string.IsNullOrEmpty(searchTerm))
             {
-                ViewModel = new Top10MoviesListViewModel()
+                viewModel = new MoviesViewModel()
                 {
-                    Movies = _movieApiService.GetMoviesBySearchTermAsync(searchTerm).Result
-                    //Movie = _movieRepository.GetMovieByImdbIdAsync("tt13143964").Result
+                    Movies = _movieApiService.GetMoviesBySearchTermAsync(searchTerm).Result,
+                    SearchTerm = searchTerm
                 };
             }
-
-            return View(ViewModel);
+            return View(viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Top10Movies.Web/Models/ViewModels/MoviesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Top10Movies.Web.Models.Core;
+
+namespace Top10Movies.Web.Models.ViewModels
+{
+    public class MoviesViewModel
+    {
+        public IEnumerable<Movie> Movies { get; set; }
+        public string SearchTerm { get; set; }
+    }
+}

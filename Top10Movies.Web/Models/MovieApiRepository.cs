76d683342cb04c39f503b1ae216ee2be95698c5e
Ryan <nolanr6969@gmail.com>
2020-11-02 13:27:18 +0000

    Moved ApiKey into constructor for movie api repository.

--- Top10Movies.Web/Models/MovieApiRepository.cs
+++ Top10Movies.Web/Models/MovieApiRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Top10Movies.Web.Models.Core;
@@ -12,15 +13,16 @@ namespace Top10Movies.Web.Models
     public class MovieApiRepository : IMovieApiService
     {
         private readonly IConfiguration _config;
-
+        private readonly string _apiKey;
         public MovieApiRepository(IConfiguration config)
         {
             _config = config;
+            _apiKey = config["MoviesApiKey"];
         }
         public async Task<Movie> GetMovieById(int id)
         {
             Movie m;
-            string requestUri = $"https://api.themoviedb.org/3/movie/{id}?api_key="+ _config["MoviesApiKey"];
+            string requestUri = $"https://api.themoviedb.org/3/movie/{id}?api_key="+ _apiKey;
             using (var httpClient = new HttpClient())
             {
                 using (var response = await httpClient.GetAsync(requestUri))

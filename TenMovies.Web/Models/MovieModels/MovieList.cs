263a45d4b61523f7b8478e838803eb89f6a240ca
Ryan <nolanr6969@gmail.com>
2020-11-12 20:45:10 +0000

    Added validation to entity props.

--- TenMovies.Web/Models/MovieModels/MovieList.cs
+++ TenMovies.Web/Models/MovieModels/MovieList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using TenMovies.Web.Models.User;
 
 namespace TenMovies.Web.Models.MovieModels
@@ -7,7 +8,11 @@ namespace TenMovies.Web.Models.MovieModels
     public class MovieList
     {
         public int Id { get; set; }
+        [MaxLength(30)]
         public string Title { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
         public Guid UserId { get; set; }
     }
 }
